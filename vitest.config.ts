cfe6fa6eef396079de83a3d03b8a6077278e78cb
VictorErbs <victorerbs2004@gmail.com>
2026-09-02 13:30:24 -0300

    fix: remove jsdom from vitest config since tests are pure ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,9 +3,7 @@ import { resolve } from 'path'
 
 export default defineConfig({
   test: {
-    environment: 'jsdom',
     globals: true,
-    setupFiles: ['./vitest.setup.ts'],
     alias: {
       '@': resolve(__dirname, './src')
     }
